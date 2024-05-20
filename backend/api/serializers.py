b2d56b7bd48cb06fa2ab5abd2a5405cdb0621e37
Sergey Dayneko, Ph.D <s.daineko@gmail.com>
2024-05-19 19:46:00 -0700

    Building The Home Page

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,14 +7,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_only": True}} # only read
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
+        print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
-    
+
+
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = None
+        model = Note
         fields = ["id", "title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
